694a9f56ca7046208ac7049a74d534a2227f0cd9
Scott Gall <scottarthurgall@gmail.com>
2018-12-27 18:04:34 -0600

    mostly finished

--- scripts.js
+++ scripts.js
@@ -19,7 +19,21 @@ function updateButton() {
 }
 
 function skip() {
-  console.log(this.dataset);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
 
 /* event listeners */
@@ -28,4 +42,18 @@ function skip() {
 );
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener('timeupdate', handleProgress);
 skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => {
+  range.addEventListener('change', handleRangeUpdate)
+  range.addEventListener('mousemove', handleRangeUpdate)
+}); 
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mouseout', (e) => { 
+  mousedown && scrub(e);
+  mousedown = false;
+});
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
